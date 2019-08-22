9e3e22569df859fc60b08d1369cbeeac61d49007
Svetlana-Vitkin <54097817+Svetlana-Vitkin@users.noreply.github.com>
2019-08-22 17:05:55 +0300

    Added new module

--- wikipedia-web-tests/src/test/java/com/telran/wikipedia/SearchWikiTest.java
+++ wikipedia-web-tests/src/test/java/com/telran/wikipedia/SearchWikiTest.java
@@ -1,20 +1,21 @@
-package com.telran.selenium;
+package com.telran.wikipedia;
+
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class OpenGoogleTest {
+public class SearchWikiTest {
     WebDriver driver;
+
     @Test
     public void setUp() throws InterruptedException {
         driver = new ChromeDriver();
         driver.get("https://www.google.com");
-        driver.findElement(By.name("q")).sendKeys("java");
+        driver.findElement(By.name("q")).sendKeys("https://en.wikipedia.org");
 
-       Thread.sleep(10000);
+        Thread.sleep(30000);
         driver.quit();
     }
-
 }
