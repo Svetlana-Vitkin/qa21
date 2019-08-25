1c2e943d52c7e7fccf765f374a92e6ad1096e553
Svetlana-Vitkin <54097817+Svetlana-Vitkin@users.noreply.github.com>
2019-08-25 09:51:29 +0300

    HomeWork_1(corrected)

--- wikipedia-web-tests/src/test/java/com/telran/wikipedia/SearchWikiTest.java
+++ wikipedia-web-tests/src/test/java/com/telran/wikipedia/SearchWikiTest.java
@@ -1,7 +1,7 @@
 package com.telran.wikipedia;
 
-
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -12,10 +12,11 @@ public class SearchWikiTest {
     @Test
     public void setUp() throws InterruptedException {
         driver = new ChromeDriver();
-        driver.get("https://www.google.com");
-        driver.findElement(By.name("q")).sendKeys("https://en.wikipedia.org");
-
-        Thread.sleep(30000);
+        driver.get("https://en.wikipedia.org");
+        driver.findElement(By.name("search")).click();
+        driver.findElement(By.name("search")).clear();
+        driver.findElement(By.name("search")).sendKeys("java"+ Keys.ENTER);
+        Thread.sleep(10000);
         driver.quit();
     }
 }
